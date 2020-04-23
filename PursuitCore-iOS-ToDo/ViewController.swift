b4e77fdb8c44a6a90fde81cbf5c4a12e1770a060
Eric Widjaja <ericwidjaja@pursuit.org>
2020-04-23 16:33:47 -0400

    add gitignore file

---
+++ PursuitCore-iOS-ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PursuitCore-iOS-ToDo
+//
+//  Created by Eric Widjaja on 4/23/20.
+//  Copyright © 2020 ericW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
