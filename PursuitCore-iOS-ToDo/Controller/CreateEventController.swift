054a61f22a313fa36229b84c7ef8f2300bb9534d
Eric Widjaja <ericwidjaja@pursuit.org>
2020-04-24 22:24:45 -0400

    connect all the IBActions and IBOutlets to VCs

--- PursuitCore-iOS-ToDo/Controller/CreateEventController.swift
+++ PursuitCore-iOS-ToDo/Controller/CreateEventController.swift
@@ -10,16 +10,23 @@ import UIKit
 class CreateEventController: UIViewController {
 
 //MARK: - Properties
+    //This is the event that goes back in the sequeway
+    var event: Event? //default value is nill
     
     
 //MARK: - IBActions and IBOutlets
 
     @IBOutlet weak var createEventTextField: UITextField!
-
+    @IBOutlet weak var datePicker: UIDatePicker!
         
         
         
-//MARK: - Regular Functions
+//MARK: - Functions
+    @IBAction func datePickerSelected(sender: UIDatePicker) {
+        //whenever the date is selected, update the event's date
+        event?.date = sender.date
+        
+    }
     
     
 //MARK: - Lifecycle
@@ -27,10 +34,7 @@ class CreateEventController: UIViewController {
         super.viewDidLoad()
         //set the VC as the delegate for the textField
         createEventTextField.delegate = self
-        
-    
-        
-        
+  
     }
 }
 
@@ -39,7 +43,10 @@ extension CreateEventController: UITextFieldDelegate {
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         //dismiss the keyboard
         textField.resignFirstResponder()
+        event?.name = textField.text ?? "No Event Name yet"
         
+        
+        //
         return true
     }
 }
--- PursuitCore-iOS-ToDo/Controller/ScheduleListController.swift
+++ PursuitCore-iOS-ToDo/Controller/ScheduleListController.swift
@@ -18,20 +18,22 @@ class ScheduleListController: UIViewController {
 //MARK: - IBActions and IBOutlets
     @IBOutlet weak var toDoTableView: UITableView!
     //Data that we are putting inside TableView is -> an array of events, we need to create Event.swift Model File. Why swift? because we are not subclassing anything, we do not to use UIKit that available as in CocoaTouch Class.
-//MARK: - Internak Properties
-//    var events = [Event]() {
-//        //        didSet {//property observer, whenever there's new data, it will reload and return the data into tableview
-//        //            toDoTableView.reloadData()}
-//        //BUT We are using different method, we need to insert either on top or bottom of the tblView
-//    }
-    
+//MARK: - Internal Properties
+    var events = [Event]()
+/*var events = [Event]() {
+        //        didSet {//property observer, whenever there's new data, it will reload and return the data into tableview
+        //            toDoTableView.reloadData()}
+        //BUT We are using different method, we need to insert either on top or bottom of the tblView
+    }
+*/
     @IBAction func addNewEvent(segue: UIStoryboardSegue) {
-        // get reference to the CreateEventController, using sequeway property from addNewEvent. where we are coming from? so use segue.source
-        guard let createEventController = segue.source as? CreateEventController else {//if we do not get CreateEventController we failed
+        // get reference to the CreateEventController, using sequeway property from addNewEvent. where we are coming from? so use segue.source and guard them
+        guard let createEventController = segue.source as? CreateEventController,
+            let createdEvent = createEventController.event else {//if we do not get CreateEventController we failed
             fatalError("Failed to Access CreateEventController")
-            
         }
-
+        //After getting the values of the variables, we can insert this new created event into the events array
+        events
     }
     
 //MARK: - Regular Functions
